557a4856f7a8a4e53c07c19b4c6412b75142c949
김민지 <9mjkim@gmail.com>
2022-06-15 09:23:29 +0900

    feat: 검색 및 페이징 수정
    list에서 검색 시 paging 값 그대로 가져가는 것 수정
    page = 1로 지정
    
    전체 리스트 불러오는 쿼리 수정
    페이징 관련 limit 잘못 사용하여 다음 페이지로 넘어가면 갈수록 글이 더 많이
    보이고 있었음. 해당 사항 수정

--- src/main/java/com/eleven/board/controller/FileController.java
+++ src/main/java/com/eleven/board/controller/FileController.java
@@ -11,9 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.print.attribute.standard.Media;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/download")
@@ -29,19 +29,18 @@ public class FileController {
     @RequestMapping("/{fileName}")
     public ResponseEntity<Resource> resourceFileDownload(@PathVariable String fileName) {
         try {
-            Resource resource = resourceLoader.getResource("file:///C:/Devroot/temp/" + fileName);
+            Resource resource = resourceLoader.getResource("file:/C:/Devroot/temp/" + fileName);
             File file = resource.getFile();
 
-
             return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + file.getName() + "\"")
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
                     .header(HttpHeaders.CONTENT_LENGTH, String.valueOf(file.length()))
-                    .contentType(MediaType.APPLICATION_PDF)
+                    .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM.toString())
                     .body(resource);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             return ResponseEntity.badRequest().body(null);
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
